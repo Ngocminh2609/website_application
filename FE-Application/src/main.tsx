d9757f2a7a124097a28343301cff2b01f0ef3503
Do Ngoc Minh <MinhDN39@fpt.com>
2026-02-18 18:31:14 +0700

    fix lỗi trắng màn khi build

--- FE-Application/src/main.tsx
+++ FE-Application/src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Polyfill cho SockJS/StompJS trên trình duyệt
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+if (typeof (window as any).global === 'undefined') {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (window as any).global = window;
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
